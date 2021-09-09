1952c8b7cd22d546edb7c0898c7f424991b4938b
alvansga <zralvansga97@gmail.com>
2021-09-09 21:15:52 +0700

    sesuaikan camera di tutoropencv.py, opencv_mouse.py bug

--- opencv_mouse.py
+++ opencv_mouse.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 # import HandTrackingModule as htm
+import mediapipe as mp
 import time
 import autopy
 
@@ -14,19 +15,38 @@ cap = cv2.VideoCapture(0)
 cap.set(3,wCam)
 cap.set(4,hCam)
 
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(static_image_mode=False,
+					  max_num_hands=2,
+					  min_detection_confidence=0.5,
+					  min_tracking_confidence=0.5)
+mpDraw = mp.solutions.drawing_utils
+
 pTime = 0
+cTime = 0
 
 while True:
 	success, img = cap.read()
 	
 	imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+	results = hands.process(imgRGB)
+
+	if results.multi_hand_landmarks:
+		for handLms in results.multi_hand_landmarks:
+			for id, lm in enumerate(handLms.landmark):
+				h,w,c = img.shape
+				cx,cy = int(lm.x *w), int(lm.y*h)
+				cv2.circle(img, (cx,cy), 3, (255,0,255), cv2.FILLED)
+
+			mpDraw.draw_landmarks(img, handLms. mpHands.HAND_CONNECTIONS)
+	
 	cTime = time.time()
-	
 	fps = 1 / (cTime - pTime)
-	
 	pTime = cTime
 	
-	cv2.putText(img, f'FPS:{int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0),2)
+	# cv2.putText(img, f'FPS:{int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0),2)
+	cv2.putText(img,str(int(fps)), (10,70), cv2.FONT_HERSHEY_SIMPLEX, 3, (255,0,255),3)
 	
 	cv2.imshow("Image",img)
 	
